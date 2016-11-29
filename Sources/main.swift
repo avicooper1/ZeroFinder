c409663d09ec0887335d07bab2ecfc9cc55c0e07
avicooper1 <avicooper007@gmail.com>
2016-11-29 00:27:05 -0500

    Update to script and main files.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,55 @@
+//print("Experiment 1: Binary search x^y as y gets arbitrarily larger")
+for x in 0...100{
+	if x % 2 == 0{
+		printFormattedResults(currentX: x, input: zeroBinarySearch(beginInterval: -10, endInterval: 0, testFunction: "(x^\(x)-1)", tolerance: 0.000000001, iteration: 0))
+	}
+}
+print()
 
-print(functionAsString(inputFunction: "(x^3-1)", xValue: 4))
+//print("Experiment 2: Newton search x^y as y gets arbitrarily larger")
+for x in 0...100{
+	if x % 2 == 0{
+		printFormattedResults(currentX: x, input: zeroNewtonianSearch(beginInterval: -10, testFunction: "(x^\(x)-1)", tolerance: 0.000000001, iteration: 0))
+	}
+}
+print()
+
+//print("Experiment 6: Combined search x^y as y gets arbitrarily larger")
+for x in 0...100{
+	if x % 2 == 0{
+		printFormattedResults(currentX: x, input: combinedAlgorithms(beginInterval: -10, endInterval: 0, testFunction: "(x^\(x)-1)", tolerance: 0.000000001, iteration: 0))
+	}
+}
+print()
+
+// print("Experiment 3: Binary search as begin interval get arbitrarily smaller than the root")
+// for x in 0...100{
+// 	printFormattedResults(currentX: -10 - x, input: zeroBinarySearch(beginInterval: (-10-Double(x)), endInterval: 0, testFunction: "(x^10-1)", tolerance: 0.000000001, iteration: 0))
+// }
+// print()
+
+// print("Experiment 4: Newton search as begin interval get arbitrarily smaller than the root")
+// for x in 0...100{
+// 	printFormattedResults(currentX: -10 - x, input: zeroNewtonianSearch(beginInterval: (-10-Double(x)), testFunction: "(x^10-1)", tolerance: 0.000000001, iteration: 0))
+// }
+// print()
+
+// print("Experiment 5: Binary search x^y as y gets arbitrarily larger and as begin interval get arbitrarily smaller than the root")
+// for x in 0...100{
+// 	if x % 2 == 0{
+// 		for y in 0...100{
+// 			printFormattedResults(currentX: x, input: zeroBinarySearch(beginInterval: (-10-Double(x)), endInterval: 0, testFunction: "(x^\(x)-1)", tolerance: 0.000000001, iteration: 0))
+// 		}
+// 	}
+// }
+// print()
+
+// print("Experiment 1: Newton search x^y as y gets arbitrarily larger and as begin interval get arbitrarily smaller than the root")
+// for x in 0...100{
+// 	if x % 2 == 0{
+// 		for y in 0...100{
+// 			printFormattedResults(currentX: x, input: zeroNewtonianSearch(beginInterval: (-10-Double(x)), testFunction: "(x^\(x)-1)", tolerance: 0.000000001, iteration: 0))
+// 		}
+// 	}
+// }
+// print()
